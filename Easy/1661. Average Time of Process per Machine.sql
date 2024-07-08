1cedad70785f024452fa2094bfec406198ea3911
Tasnia Tasneem Tohfaaa <tasniatasneem@iut-dhaka.edu>
2024-07-08 15:58:49 +0600

    solved 1661. Average Time of Process per Machine.sql

---
+++ Easy/1661. Average Time of Process per Machine.sql
@@ -0,0 +1,15 @@
+SELECT 
+    A1.machine_id, 
+    ROUND(AVG(A2.timestamp - A1.timestamp), 3) AS processing_time
+FROM 
+    Activity A1 
+JOIN 
+    Activity A2 
+ON 
+    A1.machine_id = A2.machine_id 
+    AND A1.process_id = A2.process_id 
+WHERE 
+    A1.activity_type = 'start' 
+    AND A2.activity_type = 'end'
+GROUP BY 
+    A1.machine_id;
