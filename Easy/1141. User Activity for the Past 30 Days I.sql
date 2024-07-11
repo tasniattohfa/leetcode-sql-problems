06fc8a401f8d1d7a5ddedc0c1e630b479655edf1
Tasnia Tasneem Tohfaaa <tasniatasneem@iut-dhaka.edu>
2024-07-11 15:09:18 +0600

    Update 1141. User Activity for the Past 30 Days I.sql

--- Easy/1141. User Activity for the Past 30 Days I.sql
+++ Easy/1141. User Activity for the Past 30 Days I.sql
@@ -1,4 +1,4 @@
 select activity_date day, count(distinct user_id) active_users
 from Activity 
-where activity_date between '2019-06-27' and '2019-07-27'
+where activity_date between '2019-06-28' and '2019-07-27'
 group by activity_date;
