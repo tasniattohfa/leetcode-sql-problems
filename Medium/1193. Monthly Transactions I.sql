d83c17851ac1610bfca162c1af037e808d43103d
Tasnia Tasneem Tohfaaa <tasniatasneem@iut-dhaka.edu>
2024-07-13 11:34:42 +0600

    Update 1193. Monthly Transactions I.sql

--- Medium/1193. Monthly Transactions I.sql
+++ Medium/1193. Monthly Transactions I.sql
@@ -5,5 +5,5 @@ select
     sum(case when state = 'approved' then 1 else 0 end) as approved_count,
     sum(amount) as trans_total_amount,
     sum(case when state = 'approved' then amount else 0 end) as approved_total_amount
-from transactions
+from Transactions
 group by month, country;
