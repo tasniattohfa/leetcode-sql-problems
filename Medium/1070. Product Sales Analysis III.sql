57a4648cb9448ba7305dd40d0612665632cbf29c
Tasnia Tasneem Tohfaaa <tasniatasneem@iut-dhaka.edu>
2024-07-16 11:20:26 +0600

    solved 1070. Product Sales Analysis III.sql

---
+++ Medium/1070. Product Sales Analysis III.sql
@@ -0,0 +1,8 @@
+select s.product_id,min(s.year) as first_year,s.quantity,s.price
+from Sales s
+where (s.product_id, s.year) in (
+    select product_id,min(year)
+    from Sales
+    group by product_id
+)
+group by s.product_id,s.quantity,s.price;
