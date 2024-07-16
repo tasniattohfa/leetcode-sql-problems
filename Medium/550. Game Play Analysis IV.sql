2ad698d1663928a82879d0e7296fd56a6ed5aa5f
Tasnia Tasneem Tohfaaa <tasniatasneem@iut-dhaka.edu>
2024-07-16 10:38:01 +0600

    Update 550. Game Play Analysis IV.sql

--- Medium/550. Game Play Analysis IV.sql
+++ Medium/550. Game Play Analysis IV.sql
@@ -1,6 +1,5 @@
 select round(sum(case when a2.event_date = date_add(a1.event_date, interval 1 day) then 1 else 0 end) * 1.0 / count(distinct a1.player_id), 2) as fraction
-from activity a1
-left join activity a2
+from Activity a1
+left join Activity a2
 on a1.player_id = a2.player_id and a2.event_date = date_add(a1.event_date, interval 1 day)
 where a1.event_date = (select min(event_date) from activity where player_id = a1.player_id);
-
